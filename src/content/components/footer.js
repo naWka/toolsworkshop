a04a7e63c7b014b17e987f716ff808dfa8c0b142
naWka <pshestakov94@gmail.com>
2020-07-02 20:31:11 +0700

    add footer

--- src/content/components/footer.js
+++ src/content/components/footer.js
@@ -3,17 +3,43 @@ import styled from 'styled-components';
 
 const Footer = () => (
     <Wrapper>
-        footer
+        <Text>Copyright © 2020 Tools Workshop</Text>
+        <LinksWrapper>
+            <TextHover>Terms of service</TextHover>
+            <TextHover>Privacy Policy</TextHover>
+        </LinksWrapper>
     </Wrapper>
 )
 
 const Wrapper = styled.div`
     height: 54px;
     background-color: #F3F6FF;
-    padding: 0px 36px 0px 36px;
+    padding: 0px 198px 0px 218px;
     display: flex;
     align-items: center;
     justify-content: space-between;
+`
+const LinksWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 226px;
+`
+const Text = styled.div`
+    font-size: 14px;
+    font-family: 'Raleway', sans-serif;
+    font-weight: 400;
+    color: #A6A4C7;
+`
+const TextHover = styled.div`
+    font-size: 14px;
+    font-family: 'Raleway', sans-serif;
+    font-weight: 400;
+    color: #A6A4C7;
+    text-decoration: underline;
+    &:hover {
+        cursor: pointer;
+     }
 `
 
 export default Footer;
